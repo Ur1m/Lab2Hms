be1111205df458f2d667add00b9796eef42c3282
rinorhyseni <rinorhyseni18@gmail.com>
2021-05-25 23:20:28 +0200

    Update DataContext.cs

--- Presistence/DataContext.cs
+++ Presistence/DataContext.cs
@@ -12,18 +12,16 @@ namespace Presistence
 
         }
         public  DbSet<prov> prova{ get; set;}
+        public DbSet<Department> Department{get;set;}
         public DbSet<Doktori> doktori{get;set;}
         
-        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>()
+                .HasKey(d => d.Department_id );
 
-        protected override void OnModelCreating(ModelBuilder builder) {
-                base.OnModelCreating(builder);
-
-                 builder.Entity<Doktori>(x => x.HasKey(aa => new { aa.Doktori_id}));
-                 
-
-            
+            modelBuilder.Entity<Doktori>()
+                .HasKey(d => d.Doktori_id );
+        }
     }
-    
-}
 }
