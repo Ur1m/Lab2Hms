9da343dd702a3b9e511d9a7a9c633516fd0f0cb5
berat422 <75537316+berat422@users.noreply.github.com>
2021-05-25 21:53:35 +0200

    //migrate

--- API/Program.cs
+++ API/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Presistence;
 
 namespace API
 {
@@ -13,10 +16,28 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+             var host =CreateWebHostBuilder(args).Build();
+            using(var scope=host.Services.CreateScope()){
+                var services=scope.ServiceProvider;
+                
+                try
+                {
+                  var   context=services.GetRequiredService<DataContext>();
+                  context.Database.Migrate();
+               
+                 Seed.SeedData(context);
+                }
+                catch(Exception ex){
+                    var logger=services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex,"error me migrim");
+
+                }
+            }
+            host.Run();
+
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
---
+++ Domain/prov.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Domain
+{
+    public class prov
+    {
+        public Guid id{get; set;}
+        public string prova2{get;set;}
+    }
+}
