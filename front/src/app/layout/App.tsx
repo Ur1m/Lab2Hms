9dfe441214e106b815140ba263f4f71b449c8aa9
Ur1m <83430756+Ur1m@users.noreply.github.com>
2021-05-26 01:24:09 +0200

    ReactFolderStructur
    Ndryshimi i struktures se folderave si dhe fshirja e fileve te panevojshme

---
+++ front/src/app/layout/App.tsx
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react'; 
+
+
+import { Navbar } from '../../Features/Nav/Navbar';
+
+
+
+function App() {
+  return ( 
+    <Fragment>
+   <Navbar/>
+    <Fragment>
+ 
+   </Fragment>
+   
+    </Fragment>
+  );
+}
+
+export default App;
