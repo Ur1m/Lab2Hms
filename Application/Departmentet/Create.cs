f5f4ae6a73969543e8ea139c6ed64054039cf7f0
rinorhyseni <rinorhyseni18@gmail.com>
2021-05-26 03:13:41 +0200

    Krijimi i kontrollerit Department dhe i Create, Read ( Departmenti)

---
+++ Application/Departmentet/Create.cs
@@ -0,0 +1,38 @@
+﻿using Domain;
+using MediatR;
+using Presistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Departmentet
+{
+    public class Create
+    {
+        public class Command : IRequest
+        { 
+            public Department Department { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                _context.Departmentet.Add(request.Department);
+
+                await _context.SaveChangesAsync();
+
+                return Unit.Value;
+            }
+        }
+    }
+}
---
+++ Application/Departmentet/List.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain;
+using System.Threading.Tasks;
+using System.Threading;
+using Presistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Departmentet
+{
+    public class List
+    {
+        public class Query : IRequest<List<Department>> { }
+
+        public class Handler : IRequestHandler<Query, List<Department>>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Department>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _context.Departmentet.ToListAsync();
+            }
+        }
+    }
+}
