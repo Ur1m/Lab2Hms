7c3478f6a3b1d736095804f7723d9e0a37655048
rinorhyseni <rinorhyseni18@gmail.com>
2021-06-07 03:15:43 +0200

    Revert "Create IFatura.tsx"
    This reverts commit 83da7ce24acb18f56dce08491b2337b626751b01.

--- front/src/app/models/IFatura.tsx
+++
@@ -1,9 +0,0 @@
-export interface IFatura{
-    fatura_id:string;
-    titulli:string;
-    pershkrimi:string;
-    shuma:number | null;
-    krijuarme?:Date | null;
-    statusi:string;
-    pacienti_id:string;
-}
