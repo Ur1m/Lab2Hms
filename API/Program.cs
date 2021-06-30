d63e8b62d4bf06878d6632ea1f9afe267cd6163c
rinorhyseni <rinorhyseni18@gmail.com>
2021-06-30 17:09:37 +0200

    Seed users

--- API/Program.cs
+++ API/Program.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,7 +16,7 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
              var host =CreateWebHostBuilder(args).Build();
             using(var scope=host.Services.CreateScope()){
@@ -22,10 +24,10 @@ namespace API
                 
                 try
                 {
-                  var   context=services.GetRequiredService<DataContext>();
-                 context.Database.Migrate();
-               
-                Seed.SeedData(context);
+                var   context=services.GetRequiredService<DataContext>();
+                var userManager = services.GetRequiredService<UserManager<AppUser>>();
+                await context.Database.MigrateAsync();
+                await Seed.SeedData(context, userManager);
                 }
                 catch(Exception ex){
                     var logger=services.GetRequiredService<ILogger<Program>>();
