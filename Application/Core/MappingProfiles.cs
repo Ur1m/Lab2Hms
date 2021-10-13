bfe5104d5d879a984c8181ba2d8c42528d60a996
rinorhyseni <rinorhyseni18@gmail.com>
2021-10-13 22:56:34 +0200

    final

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -15,7 +15,13 @@ namespace Application.Core
             CreateMap<Therapy ,Therapy>();
             CreateMap<Barna,Barna>();
             CreateMap<Paisjet,Paisjet>();
-             CreateMap<Terminet,Terminet>();
+            CreateMap<Terminet,Terminet>();
+            CreateMap<Fatura, Fatura>();
+            CreateMap<Department, Department>();
+            CreateMap<llojiShtratit, llojiShtratit>();
+            CreateMap<Shtrat, Shtrat>();
+            CreateMap<caktoShtratin, caktoShtratin>();
+            CreateMap<Dhoma, Dhoma>();
         }
     }
 }
